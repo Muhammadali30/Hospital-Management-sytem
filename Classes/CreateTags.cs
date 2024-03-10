5be23f0de59e7807392aaf60ad6f38514724f5ba
muhammadali30 <mirzaiffi1@gmail.com>
2024-03-10 21:58:56 +0500

    Lab report page

---
+++ Classes/CreateTags.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Final_Project.Classes
+{
+   static class CreateTags
+    {
+        public static StackPanel create_stackpanel(string? name = null, string? orientation = null, double? width = null) {
+            StackPanel stackPanel = new StackPanel();
+            stackPanel.Orientation = (Orientation)Enum.Parse(typeof(Orientation), orientation ?? "Vertical", true);
+            stackPanel.Name = name;
+            stackPanel.Width = width.HasValue ? width.Value : Double.NaN;
+            return stackPanel;
+        }
+        public static WrapPanel create_wrappanel(string? orientation = null, double? width = null)
+        {
+            WrapPanel wrapPanel = new WrapPanel();
+            wrapPanel.Width = width ?? Double.NaN;
+            wrapPanel.Orientation = (Orientation)Enum.Parse(typeof(Orientation), orientation ?? "Vertical", true);
+            return wrapPanel;
+        }
+        public static TextBlock create_textblock(string text)
+        {
+            TextBlock textblock = new TextBlock();
+            textblock.Text = text;
+            return textblock;
+        }
+        public static TextBox create_textbox(string text)
+        {
+            Style textBoxStyle = (Style)Application.Current.FindResource("textboxstyle");
+            TextBox textbox = new TextBox();
+            textbox.Text = text;
+            return textbox;
+        }
+    }
+}
--- Forms/Laboratory/InnerPages/Labreports/LabReportResultPage.xaml.cs
+++ Forms/Laboratory/InnerPages/Labreports/LabReportResultPage.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using Application = System.Windows.Application;
 
 namespace Final_Project.Forms.Laboratory.InnerPages.Labreports
 {
@@ -25,53 +27,61 @@ namespace Final_Project.Forms.Laboratory.InnerPages.Labreports
         public LabReportResultPage(int id,string name)
         {
             InitializeComponent();
-            CreateWrapPanel();
+            //CreateWrapPanel();
             Database db=new Database();
             DataTable dt = db.Read($"select * from Lab_Invoice where id={id}");
             if (dt.Rows.Count > 0)
             {
-                // Get the patient ID from the first row (index 0)
                 patientname.Text = name;
                 patientid.Text = dt.Rows[0]["unregistered_patient_id"].ToString();
+                DataTable dtt = db.Read($"SELECT template_id From Invoice_Items where id={id}");
+                foreach (DataRow row in dtt.Rows)
+                {
+                    StackPanel sp = CreateTags.create_stackpanel(null);
+                    WrapPanel wp = CreateTags.create_wrappanel("Horizontal");
+                    TextBlock tb = CreateTags.create_textblock(row["template_id"].ToString());
 
-                // Now you have the patient ID in the variable patientId
+                    StackPanel spname = CreateTags.create_stackpanel(null, "Vertical");
+                    spname.Children.Add(CreateTags.create_textblock("Name"));
+                    StackPanel spunit = CreateTags.create_stackpanel(null, "Vertical");
+                    spunit.Children.Add(CreateTags.create_textblock("Unit"));
+                    StackPanel spresult = CreateTags.create_stackpanel(null, "Vertical");
+                    spresult.Children.Add(CreateTags.create_textblock("Result"));
+                    StackPanel spmale = CreateTags.create_stackpanel(null, "Vertical");
+                    spmale.Children.Add(CreateTags.create_textblock("Range(Male)"));
+                    StackPanel spfemale = CreateTags.create_stackpanel(null, "Vertical");
+                    spfemale.Children.Add(CreateTags.create_textblock("Range(Female)"));
+                    StackPanel spheading = CreateTags.create_stackpanel(null, "Vertical");
+                    spheading.Children.Add(CreateTags.create_textblock("SubHeading"));
+
+                    wp.Children.Add(spname);
+                    wp.Children.Add(spunit);
+                    wp.Children.Add(spresult);
+                    wp.Children.Add(spmale);
+                    wp.Children.Add(spfemale);
+                    wp.Children.Add(spheading);
+
+                    sp.Children.Add(tb);
+                    sp.Children.Add(wp);
+
+                    TestReportFieldsPanel.Children.Add(sp);
+
+                    DataTable fields_table = db.Read($"select * From Lab_Templates_Fields where template_id=14");
+                    foreach (DataRow f_row in fields_table.Rows)
+                    {
+                    //var field = new { name = f_row["name"].ToString(), unit = f_row["unit"].ToString(), result = 0.ToString(), mrange = f_row["mrange"].ToString(), frange = f_row["frange"].ToString(), subheading = f_row["subheading"].ToString() };
+                        spname.Children.Add(CreateTags.create_textbox(f_row["name"].ToString()));
+                        spunit.Children.Add(CreateTags.create_textbox(f_row["unit"].ToString()));
+                        spresult.Children.Add(CreateTags.create_textbox(""));
+                        spmale.Children.Add(CreateTags.create_textbox(f_row["mrange"].ToString()));
+                        spfemale.Children.Add(CreateTags.create_textbox(f_row["frange"].ToString()));
+                        spheading.Children.Add(CreateTags.create_textbox(f_row["subheading"].ToString()));
+                    }
+                }
             }
-        }
-        private void CreateWrapPanel()
-        {
-            // Create WrapPanel
-            WrapPanel wrapPanel = new WrapPanel();
-            wrapPanel.Orientation = Orientation.Horizontal;
-            Grid.SetRow(wrapPanel, 4); // Assuming you want to add it to Grid Row 4
-
-            // Add WrapPanel to the parent Grid (assuming it's a Grid)
-            parentGrid.Children.Add(wrapPanel);
-            // Create and add StackPanels dynamically
-            CreateStackPanel(wrapPanel, "Test", 130);
-            CreateStackPanel(wrapPanel, "Unit", 100);
-            CreateStackPanel(wrapPanel, "Result", 100);
-            CreateStackPanel(wrapPanel, "Normal Range (Male)", 150);
-            CreateStackPanel(wrapPanel, "Normal Range (Female)", 150);
-            CreateStackPanel(wrapPanel, "SubHeading", 110);
-        }
-
-        private void CreateStackPanel(WrapPanel wrapPanel, string text, double width)
-        {
-            // Create StackPanel
-            StackPanel stackPanel = new StackPanel();
-            stackPanel.Width = width;
-
-            // Create TextBlock
-            TextBlock textBlock = new TextBlock();
-            textBlock.Text = text;
-            textBlock.Margin = new Thickness(5);
-            textBlock.FontWeight = FontWeights.Bold;
-
-            // Add TextBlock to StackPanel
-            stackPanel.Children.Add(textBlock);
-
-            // Add StackPanel to WrapPanel
-            wrapPanel.Children.Add(stackPanel);
+            //DataTable dtt = db.Read($"select ii.template_id from Invoice_Items ii LEFT JOIN Lab_Invoice i ON i.id = ii.invoice_id ");
+            //DataTable dtt = db.Read($"SELECT tf.name\r\nFROM Lab_Invoice i\r\nINNER JOIN Invoice_Items ii ON i.id = ii.invoice_id\r\nINNER JOIN Lab_Templates t ON ii.template_id = t.id\r\nINNER JOIN Lab_Templates_Fields tf ON t.id = tf.template_id\r\nWHERE i.id = {id};\r\n");
+            //MessageBox.Show(dtt.ToString());
         }
     }
 }
