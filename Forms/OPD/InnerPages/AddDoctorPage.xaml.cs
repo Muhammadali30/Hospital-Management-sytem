77e84cc6a449768e36fab39adc684db7c5db7b94
muhammadali30 <mirzaiffi1@gmail.com>
2024-03-23 15:29:18 +0500

    OPD work

--- Forms/OPD/InnerPages/AddDoctorPage.xaml.cs
+++ Forms/OPD/InnerPages/AddDoctorPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Final_Project.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,6 +38,28 @@ namespace Final_Project.Forms.HMS.InnerPages
                 // Go back to the previous page
                 NavigationService.GoBack();
             }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Database db = new Database();
+            DateTime dobDate, joiningDate;
+            if (!DateTime.TryParse(dob.Text, out dobDate) || !DateTime.TryParse(joining.Text, out joiningDate))
+            {
+                MessageBox.Show("Invalid date format");
+                return;
+            }
+
+            // Format the DateTime objects to the desired string format ('YYYY-MM-DD')
+            string dobFormatted = dobDate.ToString("yyyy-MM-dd");
+            string joiningFormatted = joiningDate.ToString("yyyy-MM-dd");
+
+            // Construct the SQL query with the formatted date strings
+            string query = $"INSERT INTO Doctors (name, dob, gender, designation, speacialist, joining, status, phone, address) " +
+                           $"VALUES ('{name.Text}', '{dobFormatted}', '{gender.Text}', '{designation.Text}', '{specialist.SelectedItem}', " +
+                           $"'{joiningFormatted}', '{status.SelectedValue}', '{phone.Text}', '{address.Text}')";
+            db.Add(query);
         }
     }
 }
