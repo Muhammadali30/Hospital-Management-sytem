1f3af264e78f2cb162d9a9545fc84be03ee09ee2
muhammadali30 <mirzaiffi1@gmail.com>
2024-04-11 11:24:34 +0500

    Purchase medicine work

--- Forms/Pharmacy/InnerPages/PurchasePage.xaml.cs
+++ Forms/Pharmacy/InnerPages/PurchasePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Final_Project.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,22 @@ namespace Final_Project.Forms.Pharmacy.InnerPages
     /// </summary>
     public partial class PurchasePage : Page
     {
+        private string med_dosage_form;
         public PurchasePage()
         {
             InitializeComponent();
+        }
+
+        private void meddosageform_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem dosage = (ComboBoxItem)meddosageform.SelectedItem;
+            if (dosage != null) { med_dosage_form = dosage.Content.ToString(); }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Database db = new Database();
+            db.Add($"INSERT INTO Med_Purchase(batch_no,voucher_no,supplier_id,med_id,no_of_boxes,units_in_box,expiry,date,unit_price,purchase_price) SELECT CONCAT('Hello',COALESCE(MAX(id),0)+1),'{Convert.ToInt32(voucher_no.Text)}','NULL','NULL','{Convert.ToInt32(noofboxes.Text)}'");
         }
     }
 }
